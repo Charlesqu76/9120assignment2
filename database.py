058bec5ed483f50374c24eb2544f777999b60a8c
zlqu <zlqu@trip.com>
2024-10-11 23:13:27 +1100

    feat: two func

--- database.py
+++ database.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python3
 import psycopg2
 
 #####################################################
-##  Database Connection
+# Database Connection
 #####################################################
 
 '''
 Connect to the database using the connection string
 '''
+
+
 def openConnection():
     # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
+
     userid = "y24s2c9120_xihe0498"
     passwd = "Hxy5463060432&"
     myHost = "awsprddbs4836.shared.sydney.edu.au"
-
 
     # Create a connection to the database
     conn = None
@@ -24,15 +25,19 @@ def openConnection():
                                     password=passwd,
                                     host=myHost)
 
+
     except psycopg2.Error as sqle:
         print("psycopg2.Error : " + sqle.pgerror)
-    
+
     # return the connection to use
     return conn
+
 
 '''
 Validate staff based on username and password
 '''
+
+
 def checkLogin(login, password):
     # acquire the connection from database
     conn = openConnection()
@@ -44,21 +49,30 @@ def checkLogin(login, password):
     result = cursor.fetchone()
     cursor.close()
     conn.close()
-    # check if the submitting details can match the user information from 
+    # check if the submitting details can match the user information from
     # the database and return the user information
     if result:
         return [result[0], result[1], result[2], result[3]]
     else:
         return None
 
+
+def list_to_dict(data, decs):
+    decs = (list(map(lambda x: x[0], decs)))
+    result = [dict(zip(decs, row)) for row in data]
+    return result
+
+
 '''
 List all the associated admissions records in the database by staff
 '''
+
+
 def findAdmissionsByAdmin(login):
     # acquire the connection from database
     conn = openConnection()
     cursor = conn.cursor()
-    # querying the related admission list 
+    # querying the related admission list
     SQL_query = "select * from findAdmissionsByadmin(%s)"
     cursor.execute(SQL_query, (login,))
     result = cursor.fetchall()
@@ -73,33 +87,66 @@ def findAdmissionsByAdmin(login):
     for row in result:
         dictionary = {}
         for i in range(len(column_name)):
-            # choose the column_name as the index and every row of the query 
+            # choose the column_name as the index and every row of the query
             # result as its value
             dictionary[column_name[i]] = row[i]
         display_list.append(dictionary)
     return display_list
 
+
 '''
 Find a list of admissions based on the searchString provided as parameter
 See assignment description for search specification
 '''
+
+
 def findAdmissionsByCriteria(searchString):
-
-    return
+    try:
+        searchString = searchString.strip()
+        conn = openConnection()
+        cursor = conn.cursor()
+        SQL_query = "SELECT * FROM findadmissionsbycriteria(%s)"
+        cursor.execute(SQL_query, (searchString,))
+        result = cursor.fetchall()
+        return list_to_dict(result, cursor.description)
+    except Exception as e:
+        print(e)
+        return []
+    finally:
+        cursor.close()
+        conn.close()
 
 
 '''
-Add a new addmission 
+Add a new addmission
 '''
-def addAdmission(type, department, patient, condition, admin):
-    
-    return
+
+
+def addAdmission(type_, department_, patient_, condition, admin):
+    print('addAdmissionaddAdmission')
+    print(type_, department_, patient_, condition, admin)
+    try:
+        conn = openConnection()
+        cursor = conn.cursor()
+        SQL_query = """
+        SELECT * FROM addadmission(%s, %s, %s, %s, %s)
+        """
+        cursor.execute(SQL_query, (type_, department_,
+                       patient_, condition, admin))
+        conn.commit()
+        result = cursor.fetchone()
+        return result[0]
+    except Exception as e:
+        print(e)
+        return False
+    finally:
+        cursor.close()
+        conn.close()
 
 
 '''
 Update an existing admission
 '''
 def updateAdmission(id, type, department, dischargeDate, fee, patient, condition):
-    
 
     return
